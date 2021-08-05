32fe7b06a913fd125b1caa356ac9a094b05aa1c8
Amine Makhlouf <amiinemakhlouf@gmail.com>
2021-08-05 10:32:54 +0100

    first commit

---
+++ app/src/main/java/com/example/moneysaver/modelClasses/ClientModelClass.kt
@@ -0,0 +1,9 @@
+package com.example.moneysaver.modelClasses
+
+class ClientModelClass
+    (
+    val username: String,
+    val password: String,
+    val salary: String,
+    val expenseLimit: String
+     )
